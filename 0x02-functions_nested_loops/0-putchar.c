2d3a1fc7c1afe12c9dd085a471d336dac54d5d86
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2023-07-19 19:51:01 -0700

    I am applying my knowledge of nested loops and functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
- * This is to printout _putchar.
+ * main - Prints putchar as a message.
  *
- * lets see how it goes
+ * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
-	printf("_putchar\n");
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
 	return (0);
-}	
+}
