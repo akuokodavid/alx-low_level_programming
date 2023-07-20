748304fc5e2b8aa5ecfd5e4c4b2ac6086f5abec2
King-David Gyamfi Akuoko <akuokodavid@gmail.com>
2023-07-19 19:26:24 -0700

    Trying out printing getchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This is to printout _putchar.
+ *
+ * lets see how it goes
+ */
+
+int main()
+{
+	printf("_putchar\n");
+
+	return (0);
+}	
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is to help me learn about functions and nested loops in c
